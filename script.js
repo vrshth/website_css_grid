fbf7b5c54e27c4a9f1bf94a39bd736c0ab971e3c
vrshth <102516147+vrshth@users.noreply.github.com>
2024-10-10 23:55:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('menu').addEventListener('click', function () {
+    var navbar= document.getElementById('navbar');
+    if (navbar.style.display === 'block') {
+        navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'block';
+    }
+})
